3b2386f23dca6f42570ca264f9c22081d333c4b8
Ezira Ashenafi <eazash22@gmail.com>
2025-04-12 12:07:32 +0300

    fix: use tsup for build

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: [
+    'src/index.ts',
+  ],
+  shims: true,
+  format: ['esm'],
+  external: [
+    '@antfu/eslint-config',
+  ],
+})
